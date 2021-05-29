89a251e496e766d729f62cbd7f8191439151d37d
wachcio <wachcio@poczta.fm>
2021-05-30 00:56:11 +0200

    Add scss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import "./css/main.scss";
+// import Vue from "vue";
+// import VueRouter from "vue-router";
+// import App from "./App";
+// import routes from "./routes/Routes";
+// import store from "./store/store";
 
-createApp(App).mount('#app')
+// const router = new VueRouter({
+//   // mode: "history",
+//   routes
+// });
+
+// Vue.use(VueRouter);
+// Vue.config.productionTip = false;
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: "#app",
+//   store,
+//   router,
+//   render: h =>
+//     h(App, {
+//       props: {
+//         endpointCurrent:
+//                "http://meteo.wachcio.pl/API/GetJSON.php?data=current"
+//       }
+//     })
+// });
+
+import "./css/main.scss";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
