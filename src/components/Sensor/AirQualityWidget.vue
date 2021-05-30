5d55b03262b2dd9471ca97789d88d077e0f87024
wachcio <wachcio@poczta.fm>
2021-05-30 04:10:25 +0200

    add scroll css in widgets

--- src/components/Sensor/AirQualityWidget.vue
+++ src/components/Sensor/AirQualityWidget.vue
@@ -2,13 +2,11 @@
   <div class="widgets">
     <iframe
       id="airly_428486066"
-      :src="'https://airly.eu/map/widget.html#w=280&h=380&m=false&i=true&d=false&ah=true&aw=false&l=pl&it=AIRLY_CAQI&us=metric&ut=celsius&lat=53.062772&lng=19.411941&id=43200'"
-      style="width: auto; height: auto; border: none"
+      :src="'https://airly.eu/map/widget.html#w=280&h=380&m=true&i=true&d=false&ah=true&aw=false&l=pl&it=AIRLY_CAQI&us=metric&ut=celsius&lat=53.062772&lng=19.411941&id=43200'"
     ></iframe>
     <iframe
       id="airly_874508336"
-      src="https://airly.org/map/widget.html#w=280&h=380&m=false&i=true&d=false&ah=true&aw=false&l=pl&it=AIRLY_CAQI&us=metric&ut=celsius&lat=53.068923&lng=19.422556&id=18800"
-      style="width: auto; height: auto; border: none"
+      src="https://airly.org/map/widget.html#w=280&h=380&m=true&i=true&d=false&ah=true&aw=false&l=pl&it=AIRLY_CAQI&us=metric&ut=celsius&lat=53.068923&lng=19.422556&id=18800"
     ></iframe>
   </div>
 </template>
@@ -29,16 +27,51 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.widgets {
+@import '../../css/_variables.scss';
+div.widgets {
   justify-content: center;
   display: flex;
   flex-direction: column;
 }
 iframe {
   max-width: 1200px;
-  width: 90vw;
+  width: 85vw;
   border: none;
   margin-bottom: 20px;
-  /* height: 180px; */
+  height: auto;
+
+  &::-webkit-scrollbar {
+    width: 9px;
+    height: 9px;
+  }
+  // &::-webkit-scrollbar-button {
+  //    width: 9px;
+  //    height: 9px;
+  // }
+  &::-webkit-scrollbar-thumb {
+    background: $textColor;
+    border: 2px solid $textColor;
+    border-radius: 24px;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: $textColor;
+  }
+  &::-webkit-scrollbar-thumb:active {
+    background: $bacgroundColor;
+  }
+  &::-webkit-scrollbar-track {
+    background: $bacgroundColor;
+    border: 1px solid $textColor;
+    border-radius: 9px;
+  }
+  &::-webkit-scrollbar-track:hover {
+    background: $bacgroundColor;
+  }
+  &::-webkit-scrollbar-track:active {
+    background: $bacgroundColor;
+  }
+  &::-webkit-scrollbar-corner {
+    background: transparent;
+  }
 }
 </style>
